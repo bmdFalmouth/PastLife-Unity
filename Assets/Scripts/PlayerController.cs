fce6ecefad9a0e46647baff2b10c767bb40b3502
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2013-12-11 01:26:16 +0000

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed=1.0f;
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim=GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetFloat("speed",Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+
+
+		if (Input.GetAxisRaw("Horizontal")>0)
+		{
+			transform.eulerAngles=new Vector2(0.0f,0.0f);
+			transform.Translate(new Vector3(1.0f,0.0f,0.0f)*speed*Time.deltaTime);
+		}
+		else if(Input.GetAxisRaw("Horizontal")<0)
+		{
+			transform.eulerAngles=new Vector2(0.0f,180.0f);
+			transform.Translate(new Vector3(1.0f,0.0f,0.0f)*speed*Time.deltaTime);
+		}
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			anim.SetBool("jump",true);
+			rigidbody2D.AddForce(new Vector2(0.0f,200.0f));
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag=="ground")
+		{
+			anim.SetBool("jump",false);
+		}
+	}
+}
